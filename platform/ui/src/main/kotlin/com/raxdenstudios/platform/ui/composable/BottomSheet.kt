e508e79901b71314639b3174de2ffdb8f359c342
Ángel Gómez <raxden.dev@gmail.com>
2026-06-29 18:38:12 +0200

    BottomSheet improved

--- platform/ui/src/main/kotlin/com/raxdenstudios/platform/ui/composable/BottomSheet.kt
+++ platform/ui/src/main/kotlin/com/raxdenstudios/platform/ui/composable/BottomSheet.kt
@@ -2,32 +2,54 @@ package com.raxdenstudios.platform.ui.composable
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import com.raxdenstudios.platform.ui.model.StringResource
+import com.raxdenstudios.platform.ui.theme.Spacing.xLarge
 
 object BottomSheet {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun Simple(
-        title: String,
-        message: String,
-        confirmButton: Pair<String, () -> Unit>,
-        dismissButton: Pair<String, () -> Unit>? = null,
         onDismissRequest: () -> Unit = {},
+        sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+        title: StringResource,
+        message: StringResource,
+        primaryButton: Pair<StringResource, () -> Unit>,
+        secondaryButton: Pair<StringResource, () -> Unit>? = null,
     ) {
-        val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+        Simple(
+            onDismissRequest = onDismissRequest,
+            sheetState = sheetState,
+            primaryButton = primaryButton,
+            secondaryButton = secondaryButton,
+        ) {
+            Label.TitleLarge(title.toText())
+            Spacer.Vertical.Small()
+            Label.BodyMedium(message.toText())
+            Spacer.Vertical.Custom(xLarge)
+        }
+    }
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun Simple(
+        onDismissRequest: () -> Unit = {},
+        sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+        primaryButton: Pair<StringResource, () -> Unit>? = null,
+        secondaryButton: Pair<StringResource, () -> Unit>? = null,
+        content: @Composable ColumnScope.() -> Unit,
+    ) {
         ModalBottomSheet(
             onDismissRequest = onDismissRequest,
             sheetState = sheetState,
@@ -35,27 +57,26 @@ object BottomSheet {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-                    .padding(bottom = 24.dp),
+                    .padding(horizontal = xLarge)
+                    .padding(bottom = xLarge),
             ) {
-                Label.TitleMedium(title)
-                Spacer(modifier = Modifier.height(8.dp))
-                Label.BodyMedium(message)
-                Spacer(modifier = Modifier.height(24.dp))
+                content()
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End,
                 ) {
-                    dismissButton?.let {
+                    secondaryButton?.let {
                         TextButton(
-                            content = { Label.BodyMedium(it.first) },
+                            content = { Label.BodyMedium(it.first.toText()) },
                             onClick = it.second,
                         )
                     }
-                    TextButton(
-                        content = { Label.BodyMedium(confirmButton.first) },
-                        onClick = confirmButton.second,
-                    )
+                    primaryButton?.let {
+                        TextButton(
+                            content = { Label.BodyMedium(it.first.toText()) },
+                            onClick = it.second,
+                        )
+                    }
                 }
             }
         }
